a2682daac6129ee86e621902d57e76b16efd69f1
saleem <saleem.coder@gmail.com>
2020-11-26 18:34:14 +0500

    till static files

---
+++ app/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\saleem-pc\desktop\django-on-docker\app\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
