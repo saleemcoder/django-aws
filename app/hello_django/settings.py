7e9b9b8ddb554e5334a9c0e158ecf0ce2aa7b777
saleem <saleem.coder@gmail.com>
2020-11-27 16:52:29 +0500

    media files

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "upload",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ USE_TZ = True
 
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
